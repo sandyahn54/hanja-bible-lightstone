cbadb8e2843e55a3b3315ba8a40d783e8231f089
Seong Yeol AHN <sandyahn002@gmail.com>
2025-04-29 14:56:44 +0900

    한자 Bible Lightstone 프로젝트 시작작

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -7,9 +7,10 @@ export default function Home() {
         <title>한자 Bible 시작</title>
       </Head>
       <main className="p-6 text-center text-xl">
-        <h1 className="text-3xl font-bold">한자 Bible 프로젝트에 오신 것을 환영드립니다 !</h1>
-        <p className="mt-4">표제어 0001 丶부터 자동 배포 시작합니다.</p>
+        <h1 className="text-3xl font-bold">한자 Bible 프로젝트에 오신 것을 환영합니다!</h1>
+        <p className="mt-4">표제어 0001부터 자동 배포 시작합니다.</p>
       </main>
     </>
-  )
+  );
 }
+
